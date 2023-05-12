eb6f3361d4e5fa8a5e0d5455baf6984e6be269c7
obiwan <timothy.rohr@gmail.com>
2023-05-12 07:57:53 -0400

    remove log file

--- load_webpage.py
+++ load_webpage.py
@@ -1,18 +1,21 @@
 # This script autoloads and closes a webpage. It can be integrated to test 
-# functionality of a webpage. The example checks to ensure "certifications"
+# functionality of a webpage. The example checks to ensure 'certifications'
 # is present on my github page. 
 
 from selenium import webdriver
 from time import sleep
+from termcolor import colored
 
 # Define words to find
-findme = ["Bio", "Certifications", "Connect with me", "Languages and Tools"]
+findme = ['Bio', 'Certifications', 'Connect with me', 'Languages and Tools']
 for i in findme:
     driver = webdriver.Firefox()
-    driver.get("https://github.com/TimRohr")
+    driver.get('https://github.com/TimRohr')
     sleep(3)
     if i in driver.page_source:
-        print(i, " was found on the page")
+        print(colored(i + ' - Found', 'green'))
+    else:
+        print(colored(i + ' was not Found', 'red'))
     # If writting unit tests, you would want something like the below commented line instead:
     # assertTrue(i in drive.page_source)
     driver.close()
