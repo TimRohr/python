78876c3c2ca143ca1f1ff708f2e3784adc3dfac8
obiwan <timothy.rohr@gmail.com>
2023-05-17 09:43:04 -0400

    adding window

--- madlib.py
+++ madlib.py
@@ -1,1 +1,10 @@
 from tkinter import *
+
+# Create Display Window
+w = Tk()
+w.geometry('600x400')
+w.title('Mad Libs Generator')
+Label(w, text= 'Obiwan\'s Mad Libs' , font = 'arial 20 bold').pack()
+Label(w, text = 'Select your story:', font = 'arial 15 bold').pack()
+
+w.mainloop()
