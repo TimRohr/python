4ad2955823947e1e78bd24ae16959b7e49b7da85
obiwan <timothy.rohr@gmail.com>
2023-05-17 12:09:40 -0400

    adjusting color scheme

--- madlib.py
+++ madlib.py
@@ -3,8 +3,9 @@ from tkinter import *
 # Create Display Window
 w = Tk()
 w.geometry('600x400')
-w.title('Mad Libs Generator')
-Label(w, text= 'Obiwan\'s Mad Libs' , font = 'arial 20 bold').pack()
-Label(w, text = 'Select your story:', font = 'arial 15 bold').pack()
+w.title('Mad Libs App')
+Label(w, text= 'Obiwan\'s Mad Libs' , font = 'arial 20 bold', bg='black', fg='green2').pack()
+Label(w, text = 'Select your story:', font = 'arial 15 bold', bg='black', fg='green2').pack()
+w.configure(bg='black')
 
 w.mainloop()
